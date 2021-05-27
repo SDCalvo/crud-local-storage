12d59ba9d455c1cd03469926b5ac9528a416be3b
calvix1 <santiago.d.calvo@gmail.com>
2021-05-27 19:19:13 -0300

    "correcion, habia sobreescrito los archivos con otros archivos"

---
+++ script.js
@@ -0,0 +1,74 @@
+let songs = [];
+
+let entryIndex = 0;
+
+songs = JSON.parse(localStorage.getItem("songs"));
+
+function listarCanciones() {
+
+    let songsJSON = JSON.stringify(songs);
+    localStorage.setItem("songs", songsJSON || []);
+    let cont = document.getElementById("canciones");
+    cont.innerHTML = "";
+
+    songs.forEach((item, index) => {
+
+        cont.innerHTML += `
+        <tr>
+            <th scope="row">${index}</th>
+            <td>${item.name}</td>
+            <td>${item.band}</td>
+            <td>${item.year}</td>
+            <td>
+                <button type="button" class="ms-2 btn btn-primary" data-bs-toggle="modal" onclick="updateIndex(${index})" data-bs-target="#editSong">
+                    Editar cancion
+                </button></td>
+            <td><button type="button" class="btn btn-secondary" onclick="borrarCancion(${index})">Delete</button></td>
+        </tr>`
+    })
+}
+
+function agregarCancion() {
+
+    const name = document.querySelector("#Nombre").value;
+    const band = document.querySelector("#Banda").value;
+    const year = document.querySelector("#Año").value;
+
+    songs.push({
+
+        name: name,
+        band: band,
+        year: year,
+    });
+
+    listarCanciones();
+}
+
+function borrarCancion(index) {
+
+    songs.splice(index, 1);
+    listarCanciones();
+}
+
+function editarCancion() {
+
+    const name = document.querySelector("#NombreEdit").value;
+    const band = document.querySelector("#BandaEdit").value;
+    const year = document.querySelector("#AñoEdit").value;
+
+    songs[entryIndex] = {
+
+        name: name,
+        band: band,
+        year: year,
+    };
+
+    listarCanciones();
+}
+
+function updateIndex(index) {
+
+    entryIndex = index;
+}
+
+listarCanciones();
