83e698d9ac1280bb92bd05490c133a7512ef4d9a
calvix1 <santiago.d.calvo@gmail.com>
2021-06-10 20:22:15 -0300

    "corregido el filtro"

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-let songs = [];
-
 let entryIndex = 0;
 
-songs = (JSON.parse(localStorage.getItem("songs")) || []);
+const songs = (JSON.parse(localStorage.getItem("songs")) || []);
 
 function listarCanciones() {
 
-    let songsJSON = JSON.stringify(songs);
-    localStorage.setItem("songs", songsJSON || []);
-    let cont = document.getElementById("canciones");
+    const songsJSON = JSON.stringify(songs);
+    localStorage.setItem("songs", songsJSON || "");
+    const cont = document.getElementById("canciones");
     cont.innerHTML = "";
 
-    console.log(songs)
     songs.forEach((item, index) => {
 
         cont.innerHTML += `
@@ -22,11 +19,11 @@ function listarCanciones() {
             <td class="table-text">${item.year}</td>
             <td class="d-flex justify-content-center align-items-center"><img class="logo-img" src="${item.img}" alt=""></td>
             <td>
-                <div class="d-flex justify-content-center align-items-center buttons">
+                <div class="d-flex justify-content-center align-items-center btnH buttons">
                     <button type="button" class="m-2 btn btn-primary" data-bs-toggle="modal" onclick="updateIndex(${index})" data-bs-target="#editSong">
                         Editar
                     </button>
-                    <button type="button" class="m-2 btn btn-secondary" onclick="borrarCancion(${index})">Delete</button>
+                    <button type="button" class="m-2 btn btn-secondary" onclick="borrarCancion(${index})">Eliminar</button>
                 </div>
             </td>
         </tr>`
@@ -35,10 +32,10 @@ function listarCanciones() {
 
 function agregarCancion() {
 
-    const name = document.querySelector("#Nombre").value;
-    const band = document.querySelector("#Banda").value;
-    const year = document.querySelector("#Año").value;
-    const img = document.querySelector("#Img").value;
+    const name = document.querySelector("#Nombre").value,
+        band = document.querySelector("#Banda").value,
+        year = document.querySelector("#Año").value,
+        img = document.querySelector("#Img").value;
 
     if (name == null || name == undefined || name == "") {
 
@@ -75,14 +72,19 @@ function borrarCancion(index) {
     listarCanciones();
 }
 
+function updateIndex(index) {
+
+    entryIndex = index;
+}
+
 function editarCancion() {
 
-    const name = document.querySelector("#NombreEdit").value;
-    const band = document.querySelector("#BandaEdit").value;
-    const year = document.querySelector("#AñoEdit").value;
-    const img = document.querySelector("#ImgEdit").value;
-    const modal = document.getElementById("editSong");
-    const myModal = new bootstrap.Modal(modal);
+    const name = document.querySelector("#NombreEdit").value,
+        band = document.querySelector("#BandaEdit").value,
+        year = document.querySelector("#AñoEdit").value,
+        img = document.querySelector("#ImgEdit").value,
+        modal = document.getElementById("editSong"),
+        myModal = new bootstrap.Modal(modal);
 
     if (name == null || name == undefined || name == "") {
 
@@ -116,8 +118,6 @@ function editarCancion() {
     };
 
     listarCanciones();
-
-
 }
 
 function validURL(str) {
@@ -130,53 +130,41 @@ function validURL(str) {
     return !!pattern.test(str);
 }
 
-function updateIndex(index) {
-
-    entryIndex = index;
-}
-
 /* Busqueda */
-
-const searchBtn = document.getElementById("searchBtn"),
-    searchInput = document.getElementById("searchInput"),
-    result = document.getElementById("searchResult");
 
 function searchItem() {
 
-    let value = searchInput.value;
-    let searchRes = songs.filter(item => value === item.name || value === item.band || value === item.year);
+    const value = document.getElementById("searchInput").value,
+        cont = document.getElementById("canciones"),
+        searchRes = songs.filter(item => value === item.name || value === item.band || value === item.year);
 
-    result.innerHTML = "";
+    if (value == "" || value == null || value == undefined) {
 
-    if (searchRes == "" || searchRes == undefined || searchRes == null) {
-
-        result.innerHTML += ` 
-                        <div class="container border searchMargin">
-                            <div class="row justify-content-center align-items-center text-center flex-column">
-                                <p class="p-2 text-center">No se encontró un resultado que coincida con tu búsqueda</p>
-                            </div>
-                        </div>
-            `
+        alert("No se encontraron resultados que coincidan con tu búsqueda");
 
     } else {
 
+        cont.innerHTML = "";
         searchRes.forEach((item, index) => {
 
-            result.innerHTML += ` 
-                        <div class="container border searchMargin">
-                            <div class="row justify-content-center align-items-center text-center flex-column">
-                                <p class="p-2">Cancion: ${item.name}</p>
-                                <p class="p-2">Banda: ${item.band}</p>
-                                <p class="p-2">Año: ${item.year}</p>
-                                <div class="img-fluid">
-                                    <img class="logo-img" src="${item.img}">
-                                </div>
-                            </div>
-                        </div>
-            `
+            cont.innerHTML += `
+            <tr>
+                <th class="table-text" scope="row">${index}</th>
+                <td class="table-text">${item.name}</td>
+                <td class="table-text">${item.band}</td>
+                <td class="table-text">${item.year}</td>
+                <td class="d-flex justify-content-center align-items-center"><img class="logo-img" src="${item.img}" alt=""></td>
+                <td>
+                    <div class="d-flex justify-content-center align-items-center buttons">
+                        <button type="button" class="m-2 btn btn-primary" data-bs-toggle="modal" onclick="updateIndex(${index})" data-bs-target="#editSong">
+                            Editar
+                        </button>
+                        <button type="button" class="m-2 btn btn-secondary" onclick="borrarCancion(${index})">Delete</button>
+                    </div>
+                </td>
+            </tr>`
         })
     }
-
 }
 
 searchBtn.addEventListener("click", () => {
